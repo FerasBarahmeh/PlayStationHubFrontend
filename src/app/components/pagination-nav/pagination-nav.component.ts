744455e61a7baeea8046b79b5253878ea633eb41
FerasBarahmeh <FerasBarahmeh@GLADEATOR>
2024-10-09 21:24:32 +0300

    add pagination style and diplay users to admin/users pages

--- src/app/components/pagination-nav/pagination-nav.component.ts
+++ src/app/components/pagination-nav/pagination-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
@@ -17,11 +17,13 @@ export class PaginationNavComponent implements OnInit {
   public icons = {
     faAngleLeft,
     faAngleRight,
-  }
+  };
 
   public pageNumber: number = 1;
 
   public pageSize!: number;
+
+  @Output() onSlideChangeEvent: EventEmitter<{ pageNumber: number, pageSize: number }> = new EventEmitter();
 
   constructor() { }
 
@@ -30,14 +32,28 @@ export class PaginationNavComponent implements OnInit {
     this.pageSize = temp ? Number(temp) : 1;
   }
 
-  public onChangeSlide(newSlide: number) {
-    this.pageNumber = newSlide <= 0 ? 1 : newSlide;
+  private selectSlide(slide: number) {
+    if (slide >= 1 && slide <= 100) {
+      this.pageNumber = slide;
+      this.onSlideChangeEvent.emit({ pageNumber: this.pageNumber, pageSize: this.pageSize });
+    }
+  }
+
+  public previousSlide(): void {
+    if (this.pageNumber > 1) {
+      this.selectSlide(this.pageNumber - 1);
+    }
+  }
+
+  public nextSlide(): void {
+    if (this.pageNumber < 100) {
+      this.selectSlide(this.pageNumber + 1);
+    }
   }
 
   public onPageSizeChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     this.pageSize = Number(selectElement.value);
-
     localStorage.setItem('pageSize', this.pageSize.toString());
   }
 }
