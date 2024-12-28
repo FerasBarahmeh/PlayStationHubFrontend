a996ba35fff804d91152e88db929a8645072c502
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-12-28 12:25:19 +0300

    edit env url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:5110/api/'
+  // API_URL: 'http://localhost:5110/api/',
+  API_URL: 'http://playstationhub.runasp.net/api/'
 };
