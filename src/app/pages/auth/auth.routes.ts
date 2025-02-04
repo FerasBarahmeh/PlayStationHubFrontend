1d744b9160b069b020bb393886e5a97a5700903d
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2025-02-04 22:50:33 +0300

    change not found directory to be in layout folder

--- src/app/pages/auth/auth.routes.ts
+++ src/app/pages/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {LogoutComponent} from "./logout/logout.component";
-import {NotFoundComponent} from "../../components/not-found/not-found.component";
+import {NotFoundComponent} from "../layouts/not-found/not-found.component";
 import {UnauthorizedComponent} from "../layouts/unauthorized/unauthorized.component";
 import {isAuthGuard} from "../../guard/is.auth.guard";
 
