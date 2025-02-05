9567f3ffad4df99de23d8c2a91319bf2360465fb
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2025-02-05 23:46:35 +0300

    add admin-layout in club page (for admin)

--- src/app/pages/admins/club/club.component.ts
+++ src/app/pages/admins/club/club.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {AdminDashboardComponent} from "../admin-dashboard/admin-dashboard.component";
 import {ClubComponent as CC} from "../../../components/club/club.component";
 import {ActivatedRoute} from "@angular/router";
 import {ClubsService} from "../../../services/clubs.service";
 import {IClub} from "../../../interfaces/clubs/IClub";
 import {IResponse} from "../../../interfaces/responses/IResponse";
+import {AdminComponent} from "../../layouts/admin/admin.component";
 
 @Component({
   selector: 'admin-club',
   standalone: true,
-  imports: [AdminDashboardComponent, CC],
+  imports: [CC, AdminComponent],
   templateUrl: './club.component.html',
   styleUrl: './club.component.css'
 })
